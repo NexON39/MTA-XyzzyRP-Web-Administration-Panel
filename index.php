32b31e96bd726c64dd26c1599ea8b0ca61a4e7f3
Kacper <kscode.pl@gmail.com>
2022-02-04 18:51:33 +0100

    add login subpage

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/animation.css">
+    <link rel="stylesheet" href="css/utilities.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/responsives.css">
+</head>
+<body>
+    <!-- main -->
+    <div class="loginpanel">
+        <div class="container">
+            <!-- input -->
+            <div class="inputs loginpanel">
+                <div class="text-header">
+                    <h1>XyzzyRP Administration Panel</h1>
+                </div>
+                <form action="" method="post">
+                    <div><input type="text" placeholder="Nick"></div>
+                    <div><input type="password" placeholder="Hasło"></div>
+                    <div><input type="submit" value="Zaloguj"></div>
+                </form>
+            </div>
+
+    <script src="js/aos.js"></script>
+    <script src="js/app.js"></script>
+
+</body>
+</html>
