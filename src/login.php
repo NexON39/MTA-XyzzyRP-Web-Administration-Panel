75ad05357c5ba560254ebaa380faf19b09e5dfc2
Kacper <kscode.pl@gmail.com>
2022-03-04 21:03:32 +0100

    php update

--- src/login.php
+++ src/login.php
@@ -11,24 +11,28 @@
             $login = htmlentities($login, ENT_QUOTES, "UTF-8");
             $pass = htmlentities($pass, ENT_QUOTES, "UTF-8");
 
-            $hash_pass = password_hash($pass, PASSWORD_DEFAULT);
+            $now = date("Y-m-d H:i:s");
 
                 if($res = $con->query(sprintf("SELECT * FROM xyzzyrp_users WHERE user='%s'",
                 mysqli_real_escape_string($con,$login)))) {
                     if($res->num_rows==1) {
                         $row = $res->fetch_assoc();
-                        if(password_verify(mysqli_real_escape_string($con,$pass),$hash_pass)) {
+                        if(password_verify(mysqli_real_escape_string($con,$pass),$row['pass'])) {
                             unset($_SESSION['alert']);
                             $_SESSION['logged'] = true;
                             $_SESSION['user'] = $row['user'];
-                            $_SESSION['alert'] = "
-                            <div class='alertbox'>
-                                <p>Pomyślnie zalogowano</p>
-                                <div class='closebtn'>&times;</div>
-                            </div>";
+                            $con->query("INSERT INTO xyzzyrp_logs(`id`,`user`,`action`,`date`) VALUES ('NULL', '$_SESSION[user]', 'Zalogował się', '$now');");
                             header("Location: ../dashboard.php");
                             $res->free_result();
                             $con->close();
+                        } else {
+                            $_SESSION['alert'] = "
+                            <div class='alertbox'>
+                                <p>Błędny login lub hasło</p>
+                                <div class='closebtn'>&times;</div>
+                            </div>";
+                            header("Location: ../index.php");
+                            exit();
                         }
                     } else {
                         $_SESSION['alert'] = "
@@ -40,8 +44,7 @@
                         exit();
                     }
                 } 
-        } else
-            echo "Err: ".$con->connect_errno." W razie problemów skontaktuj się z twórcą panelu - Discord: NexON39#5665";
+        }
     } else {
         $_SESSION['alert'] = "
         <div class='alertbox'>
